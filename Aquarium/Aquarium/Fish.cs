1a269123343fbe4504b275b8003dcfb97459debb
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-20 16:25:11 +0200

    no message

---
+++ Aquarium/Aquarium/Fish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium
+{
+    public class Fish
+    {
+        public string Look { get; set; }
+        public string Size { get; set; }
+        public int MoveSpeedLeftAndRight { get; set; }
+        public double MoveSpeedUpAndDown { get; set; }
+
+        public Fish(string look, string size, int moveSpeedLeftAndRight, double moveSpeedUpAndDown)
+        {
+            Look = look;
+            Size = size;
+            MoveSpeedLeftAndRight = moveSpeedLeftAndRight;
+            MoveSpeedUpAndDown = moveSpeedUpAndDown;
+        }
+    }
+}
